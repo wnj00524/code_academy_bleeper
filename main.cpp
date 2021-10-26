b803a8a260c876b75ae284603960645a7774d8fe
wnj00524 <68168066+wnj00524@users.noreply.github.com>
2021-10-26 21:07:22 +0100

    Added ability to specific a wordlist file and run it against the datafile and save to a specified output.
    Added help flag.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "bleeper.h"
 
-using namespace  std;
+using namespace std;
+
+using std::string;
+using std::cout;
+using std::cin;
 
 int main(int argc, char* argv[]) {
     string word, text;
+    if (argc == 2  && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1],"-H") == 0))
+    {
+        cout << "Handy censor tool! Use without parameters to be prompted to provide text and a watch word. \n-f filename1 word = run the word against filename1. Saves output in output.txt\n-c datafile wordfile outputfile = Run the words in wordfile against datafile and save in outputfile.\n";
+        return 0;
+    }
     if (argc < 3)
     {
         string word;
@@ -19,10 +29,58 @@ int main(int argc, char* argv[]) {
         cout << text << "\n";
         return 0;
     }
-    if (argc == 4)
+    if (argc == 5)
     {
+
+        if (strcmp(argv[1],"-c") == 0)
+        {
+
+            std::ifstream  ifs(argv[2]);
+            if (ifs.good())
+            {
+
+                std::vector<std::string> words;
+                std::ifstream wfs(argv[3]);
+                std::string line;
+                if (wfs.good())
+                {
+                    string text((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
+                    while (getline(wfs,line))
+                    {
+                        words.push_back(line);
+                    }
+                    for (int a = 0; a < words.size(); a++)
+                    {
+                        bleeper(words[a],text);
+                    }
+                    ofstream out(argv[4]);
+                    if (out.good()) {
+                        out << text;
+                        out.close();
+                        std::cout << "Ran the words in " << argv[4] << " against " << argv[3] << " and saved in "
+                                  << argv[4] << "\n";
+                    }
+                    else {
+                        std::cout << "Unable to save to " << argv[4] << "\n";
+                    }
+                    return 0;
+
+                }
+                else
+                {
+                    cout << "Word list file  not found.\n";
+                    return 0;
+                }
+            }
+            else
+            {
+                std::cout << "Data file not found.\n";
+            }
+            return 0;
+
+        }
         //cout << "Got four...\nNum 1 = " << argv[1] << "\nArg 2 = " << argv[2] << "\nArg 3 = " << argv[3] << "\n";
-        if (strcmp(argv[1],"-f") == 0)
+        else if (strcmp(argv[1],"-f") == 0)
         {
             //cout << "Got to the -f check...\n";
            std::ifstream  ifs(argv[2]);
